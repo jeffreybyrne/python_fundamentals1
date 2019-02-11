e0f4f5a219bcfdacd5af44e0c4081aa666fb5fa3
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-11 13:48:36 -0500

    finished the fourth part of exercise four

--- e4p4.py
+++ e4p4.py
@@ -1,3 +1,14 @@
 #Pick a number and save it in a variable called secret_number. Ask the user
 #to enter a number. If they enter the secret number, print "You win!". If
 #they are off by 1, print "So close!". Otherwise, print "Try again".
+
+secret_number = 99
+print("Please enter a number")
+user_number = int(input())
+
+if (user_number == secret_number):
+    print("You win!")
+elif (abs(secret_number-user_number) == 1):
+    print("So close!")
+else:
+    print("Try again")
