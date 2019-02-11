e45e5007d37b7e85aca7f67e1d3480e526c9ea5e
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-11 11:50:21 -0500

    printing a string to ask for the user's name

--- exercise3.py
+++ exercise3.py
@@ -1,29 +1,32 @@
-#Copying & starting with a large part of the exercise we walked through with Natalie during today's lesson
-print("What's your name?")
-user_name = input()
-print("Hi {}!".format(user_name))
+#Copying & starting with a large part of the exercise we walked through with Natalie during today's lesson, I'll comment
+#it all out and just use it as reference
+# print("What's your name?")
+# user_name = input()
+# print("Hi {}!".format(user_name))
+#
+# print("What year were you born?")
+# user_birth_year = input()
+# user_age = 2019 - int(user_birth_year)
+# print("You are {} years old".format(user_age))
+#
+# print("What day of the week is it?")
+# day_of_week = input()
+#
+# if day_of_week.lower() == "tuesday":
+#     print("Happy Tuesday!")
+#     print("Do you like Tuesdays?")
+#     like_tues = input()
+#     if like_tues.lower() == 'yes' or like_tues.lower() == 'y':
+#         print("Yay!")
+#     else:
+#         print("Sorry :'(")
+# elif day_of_week.lower() == "wednesday":
+#     print("Happy Wednesday!")
+#     print("You're halfway through the week")
+# elif day_of_week.lower() == "sunday":
+#     print("Happy Sunday!")
+# else:
+#     print("Goodbye!")
+#     print("Have a good day!")
 
-print("What year were you born?")
-user_birth_year = input()
-user_age = 2019 - int(user_birth_year)
-print("You are {} years old".format(user_age))
-
-print("What day of the week is it?")
-day_of_week = input()
-
-if day_of_week.lower() == "tuesday":
-    print("Happy Tuesday!")
-    print("Do you like Tuesdays?")
-    like_tues = input()
-    if like_tues.lower() == 'yes' or like_tues.lower() == 'y':
-        print("Yay!")
-    else:
-        print("Sorry :'(")
-elif day_of_week.lower() == "wednesday":
-    print("Happy Wednesday!")
-    print("You're halfway through the week")
-elif day_of_week.lower() == "sunday":
-    print("Happy Sunday!")
-else:
-    print("Goodbye!")
-    print("Have a good day!")
+print("What is your name?")
