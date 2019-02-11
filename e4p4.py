064f6e1776a61034af20be0b89dc5881ce65db46
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-11 13:46:28 -0500

    starting part four of exercise four

---
+++ e4p4.py
@@ -0,0 +1,3 @@
+#Pick a number and save it in a variable called secret_number. Ask the user
+#to enter a number. If they enter the secret number, print "You win!". If
+#they are off by 1, print "So close!". Otherwise, print "Try again".
