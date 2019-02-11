0e9a40fa1437206d63212f43f87fd7e2588eadbc
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-11 11:18:37 -0500

    started the second exercise

---
+++ exercise2.py
@@ -0,0 +1,3 @@
+#Question 1: How would you calculate a good tip for a 55 dollar meal? Use print to print the answer.
+baseCost = 55
+print("$" + str(baseCost) + " is the base cost, let's give a 20% tip of $" + str(baseCost*0.2) + " for a final cost of $" + str(baseCost*1.2)) #Let's give it a 
