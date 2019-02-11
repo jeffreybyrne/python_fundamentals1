790bc8950eda816aae25d760ce2f04082bd893a1
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-11 11:21:37 -0500

    finished the second question of the second exercise

--- exercise2.py
+++ exercise2.py
@@ -1,3 +1,7 @@
 #Question 1: How would you calculate a good tip for a 55 dollar meal? Use print to print the answer.
 baseCost = 55
-print("$" + str(baseCost) + " is the base cost, let's give a 20% tip of $" + str(baseCost*0.2) + " for a final cost of $" + str(baseCost*1.2)) #Let's give it a 
+print("$" + str(baseCost) + " is the base cost, let's give a 20% tip of $" + str(baseCost*0.2) + " for a final cost of $" + str(baseCost*1.2)) #Let's give it a
+
+#Question 2: Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use print to print the result.
+#I may have accidentally done this as part of the previous exercise, but let's do it again anyway!
+print("If we start with 10 and add 5 to it, we end up with " + str(10+5))
