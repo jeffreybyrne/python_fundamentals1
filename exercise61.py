de8e461539a1d8e079e5f7c1b9b928095ca0f5d8
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-11 14:05:23 -0500

    finished the first part of exercise six

---
+++ exercise61.py
@@ -0,0 +1,23 @@
+# Allow the user to go home when they are done exercising. The program should
+# stop asking for input if the user enters 'go home'.
+#
+# See if you can also make the program tell the user when they have entered a
+# command that does not exist.
+
+distance_home = 0
+still_out = True
+while still_out == True:
+    print("Would you like to walk, run, or go home?")
+    response = input()
+    if (response.lower() == "walk"):
+        distance_home += 1
+        print("Distance from home is {}km".format(distance_home))
+    elif (response.lower() == "run"):
+        distance_home += 5
+        print("Distance from home is {}km".format(distance_home))
+    elif (response.lower() == "go home"):
+        #Had to change what I was looping over, but now if they want
+        still_out = False
+        print("Hope you had fun on your walk! Your total distance was {}km today.".format(distance_home))
+    else: #Oops, apparently I already did this part earlier
+        print("Sorry, that's an invalid input")
